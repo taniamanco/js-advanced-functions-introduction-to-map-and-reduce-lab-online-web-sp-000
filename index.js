c7dea02c47c08710a801b66b43b9beea478882be
Tania Manco <belalov.tania16@gmail.com>
2019-11-25 20:05:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,21 @@ function mapToNegativize(arr){
   }
   return newArr;
 }
+
+function mapToNoChange(arr){
+  let newArr = []; 
+
+  for(let i = 0; i < arr.length; i++){
+   newArr.push(arr[i])
+  }
+  return newArr;
+}
+
+function mapToNegativize(arr){
+  let newArr = []; 
+
+  for(let i = 0; i < arr.length; i++){
+   newArr.push(arr[i] * -1)
+  }
+  return newArr;
+}
