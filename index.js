5e91b440c2c371e10cc3c691ae631c70e6eda85e
Tania Manco <belalov.tania16@gmail.com>
2019-11-25 20:08:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -33,3 +33,25 @@ function mapToSquare(arr){
   }
   return newArr;
 }
+
+function reduceToTotal(src, startingPoint=0) {
+  let total = startingPoint
+  for (let i = 0; i < src.length; i++ ) {
+    total = total + src[i]
+  }
+  return total
+}
+
+function reduceToAllTrue(src) {
+  for (let i = 0; i < src.length; i++ ) {
+    if (!src[i]) return false
+  }
+  return true
+}
+
+function reduceToAnyTrue(src) {
+  for (let i = 0; i < src.length; i++ ) {
+    if (src[i]) return true
+  }
+  return false
+}
