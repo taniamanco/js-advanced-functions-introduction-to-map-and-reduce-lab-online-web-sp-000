4342654c33c90c4a05cd798406fb6e00a62078ed
Tania Manco <belalov.tania16@gmail.com>
2019-11-25 20:06:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,3 +24,12 @@ function mapToDouble(arr){
   }
   return newArr;
 }
+
+function mapToSquare(arr){
+  let newArr = []; 
+
+  for(let i = 0; i < arr.length; i++){
+   newArr.push(arr[i] * -1)
+  }
+  return newArr;
+}
